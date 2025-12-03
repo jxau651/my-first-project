7357690ac0f27e659254daa7b81508f2343f602f
jxau651 <tuzhangli202267@gmail.com>
2025-12-03 13:44:37 +0800

    one

---
+++ 25.12.3.py
@@ -0,0 +1,10 @@
+M=2.9
+count=0
+while count<3:
+    N=input("Enter number:")
+    try:
+        A=M*float(N)
+        print(round(A,4))
+        count+=1
+    except ValueError:
+        print("Please enter a number!")
